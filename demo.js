1a8e1db81e7e728a10de70a3bfbeefce60522f21
kekablue <kekablue@Erickas-MacBook-Pro.local>
2016-03-16 17:22:31 -0600

    Files for the calendar project

---
+++ demo.js
@@ -0,0 +1,65 @@
+$(function()
+{
+
+	if (!window['console'])
+	{
+		window.console = {};
+		window.console.log = function(){};
+	}
+		
+	/*
+	define a new language named "custom"
+	*/
+
+	$.dateRangePickerLanguages['custom'] = 
+	{
+		'selected': 'Choosed:',
+		'days': 'Days',
+		'apply': 'Close',
+		'week-1' : 'Mon',
+		'week-2' : 'Tue',
+		'week-3' : 'Wed',
+		'week-4' : 'Thu',
+		'week-5' : 'Fri',
+		'week-6' : 'Sat',
+		'week-7' : 'Sun',
+		'month-name': ['January','February','March','April','May','June','July','August','September','October','November','December'],
+		'shortcuts' : 'Shortcuts',
+		'past': 'Past',
+		'7days' : '7days',
+		'14days' : '14days',
+		'30days' : '30days',
+		'previous' : 'Previous',
+		'prev-week' : 'Week',
+		'prev-month' : 'Month',
+		'prev-quarter' : 'Quarter',
+		'prev-year' : 'Year',
+		'less-than' : 'Date range should longer than %d days',
+		'more-than' : 'Date range should less than %d days',
+		'default-more' : 'Please select a date range longer than %d days',
+		'default-less' : 'Please select a date range less than %d days',
+		'default-range' : 'Please select a date range between %d and %d days',
+		'default-default': 'This is costom language'
+	};
+	
+		$('#calendar').dateRangePicker(
+	   {
+	
+	 
+	  beforeShowDay: function(date) {
+      
+	  var weekend = date.getDay() == 0 || date.getDay() == 6;
+	  	  
+	  var highlight = weekend;
+                if( highlight ) {
+                     return [true, "myweekend", highlight];
+
+                } else {
+                     return [true, '', ''];
+                }
+             }
+		});
+
+});
+
+
